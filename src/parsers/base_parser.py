c4b322274489ccdbbd2fa4b235cae75b4af68e15
Derek Vawdrey <derek.vawdrey@gmail.com>
2025-11-19 15:31:45 -0700

    Adding in variables

--- src/parsers/base_parser.py
+++ src/parsers/base_parser.py
@@ -11,12 +11,17 @@ class BaseParser(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def parse(self):
+    def parse(self, datapoint_size: int = None):
         """
         Parse the dataset into a compatible format for the GRPOTrainer.
 
         The GRPOTrainer expects a dataset in the following format at a minimum:
             - prompt: The prompt to be passed to the model.
 
+        Args:
+            datapoint_size: The size of the datapoint to parse. If None, parse the entire dataset.
+
+        Returns:
+            The parsed dataset.
         """
         raise NotImplementedError
