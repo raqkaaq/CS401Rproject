d8e7e7f9b32ae573620e6dad74e7dad506b01e5d
Derek Vawdrey <derek.vawdrey@gmail.com>
2025-12-01 12:58:42 -0700

    SUBMIT TRAINING NOW

--- scripts/submit_training.sh
+++ scripts/submit_training.sh
@@ -161,8 +161,8 @@ accelerate launch --mixed_precision=bf16 src/main.py \
   --batch-size 8 \
   --num-samples 5000 \
   --save-steps 25 \
-  --max-prompt-length 512 \
-  --max-completion-length 1024 \
+  --max-prompt-length 700 \
+  --max-completion-length 700 \
   --save-strategy steps \
   --logging-steps 10
 # Save options:
